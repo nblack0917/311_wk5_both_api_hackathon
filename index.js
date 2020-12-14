810b280c54dea9c8ea88fc22871a8fa4bb7ced4f
alexanderalban <albanalexander@gmail.com>
2020-12-14 11:40:11 -0600

    index.js all set up and ready to go

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const port = process.env.PORT || 4001;
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('Welcome to our API!')
+})
+
+app.listen(port, () => {
+ console.log(`Web server is listening on port ${port}!`);
+});
