3abd57bb39f1755bf47d293f3ce033c731506051
alexanderalban <albanalexander@gmail.com>
2020-12-14 15:40:51 -0600

    adjustment to join department url

--- controllers/employee-join-departments.js
+++ controllers/employee-join-departments.js
@@ -5,7 +5,7 @@ const { handleSQLError } = require('../mysql/error')
 
 const joinWithDepartments = (req, res) => {
   console.log("up")
-    let sql = "SELECT * FROM employees LEFT JOIN dept_emp ON employees.emp_no = dept_emp.emp_no LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no ORDER BY employees.emp_no LIMIT 50"
+    let sql = "SELECT employees.emp_no, employees.first_name, employees.last_name, departments.dept_name FROM employees LEFT JOIN dept_emp ON employees.emp_no = dept_emp.emp_no LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no ORDER BY employees.emp_no LIMIT 50"
     console.log(sql)
   
     pool.query(sql, (err, rows) => {
