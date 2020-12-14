2e29ae3da9be8e68d84dfc2883c0068021e46f93
nblack0917 <nickablack@gmail.com>
2020-12-14 13:28:23 -0600

    commit

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const bodyParser = require("body-parser");
 const employeesRouter = require('./routes/employees');
 
 const employeeJoinDepartmentsRouter = require('./routes/employee-join-departments.js');
-=======
-const salariesRouter = require('./routes/salaries');
-const departmentsRouter = require('./routes/departments');
+// // =======
+// const salariesRouter = require('./routes/salaries');
+// const departmentsRouter = require('./routes/departments');
 
 
 const app = express();
