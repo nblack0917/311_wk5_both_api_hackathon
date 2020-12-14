1d5c1b2d0d2f113dfe91033a54577705c5003064
nblack0917 <nickablack@gmail.com>
2020-12-14 16:16:58 -0600

    removed extra line

--- controllers/employee-join-departments.js
+++ controllers/employee-join-departments.js
@@ -4,14 +4,10 @@ const { handleSQLError } = require('../mysql/error')
 
 
 const joinWithDepartments = (req, res) => {
-  console.log("up")
-    let sql = "SELECT * FROM employees LEFT JOIN dept_emp ON employees.emp_no = dept_emp.emp_no LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no ORDER BY employees.emp_no LIMIT 50"
-    console.log(sql)
     let sql = "SELECT employees.emp_no, employees.first_name, employees.last_name, departments.dept_name FROM employees LEFT JOIN dept_emp ON employees.emp_no = dept_emp.emp_no LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no ORDER BY employees.emp_no LIMIT 50"
 
     pool.query(sql, (err, rows) => {
       if (err) return handleSQLError(res, err)
-      console.log(rows)
       return res.json(rows);
     })
   }
@@ -35,5 +31,7 @@ const departmentByName = (req, res) => {
   }
 
 module.exports = {
-  joinWithDepartments
+  joinWithDepartments,
+  departmentById,
+  departmentByName
 }
