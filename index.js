927588c00567a0d955ac90da127cafda74f58058
alexanderalban <albanalexander@gmail.com>
2020-12-14 12:10:53 -0600

    typo correction

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const employeesRouter = require('./routers/employees');
+const employeesRouter = require('./routes/employees');
 
 const app = express();
 
