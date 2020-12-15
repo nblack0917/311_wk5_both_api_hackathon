61c23ca4583b5f2c656c4297ebedcb8b33c54078
nblack0917 <nickablack@gmail.com>
2020-12-15 09:32:43 -0600

    fine tunes salaries controllers

--- controllers/join-all.js
+++ controllers/join-all.js
@@ -5,7 +5,7 @@ const { handleSQLError } = require('../mysql/error')
 
 const joinAll = (req, res) => {
   
-    let sql = "SELECT * FROM employees LEFT JOIN titles ON employees.emp_no = titles.emp_no LEFT JOIN salaries ON employees.emp_no = salaries.emp_no LEFT JOIN dept_emp ON employees.emp_no = dept_emp.emp_no LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no ORDER BY employees.emp_no LIMIT 50"
+    let sql = `SELECT employees.emp_no, employees.first_name, employees.last_name, departments.dept_name, salaries.salary FROM employees LEFT JOIN titles ON employees.emp_no = titles.emp_no LEFT JOIN salaries ON employees.emp_no = salaries.emp_no LEFT JOIN dept_emp ON employees.emp_no = dept_emp.emp_no LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no WHERE salaries.to_date LIKE "9999%" ORDER BY employees.emp_no LIMIT 50`
     console.log(sql)
   
     pool.query(sql, (err, rows) => {
@@ -17,7 +17,7 @@ const joinAll = (req, res) => {
   }
 
   const salaryById = (req, res) => {
-    let sql = `SELECT * FROM employees LEFT JOIN titles ON employees.emp_no = titles.emp_no LEFT JOIN salaries ON employees.emp_no = salaries.emp_no LEFT JOIN dept_emp ON employees.emp_no = dept_emp.emp_no LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no WHERE employees.emp_no = ${req.params.emp_no}`  
+    let sql = `SELECT employees.emp_no, employees.first_name, employees.last_name, departments.dept_name, salaries.salary FROM employees LEFT JOIN titles ON employees.emp_no = titles.emp_no LEFT JOIN salaries ON employees.emp_no = salaries.emp_no LEFT JOIN dept_emp ON employees.emp_no = dept_emp.emp_no LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no WHERE employees.emp_no = ${req.params.emp_no} AND salaries.to_date LIKE "9999%"`  
 
     pool.query(sql, (err, rows) => {
       if (err) return handleSQLError(res, err)
@@ -26,7 +26,7 @@ const joinAll = (req, res) => {
   }
 
 const salaryByName = (req, res) => {    
-    let sql = `SELECT * FROM employees LEFT JOIN titles ON employees.emp_no = titles.emp_no LEFT JOIN salaries ON employees.emp_no = salaries.emp_no LEFT JOIN dept_emp ON employees.emp_no = dept_emp.emp_no LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no WHERE employees.first_name = "${req.params.first_name}"`  
+    let sql = `SELECT employees.emp_no, employees.first_name, employees.last_name, departments.dept_name, salaries.salary FROM employees LEFT JOIN titles ON employees.emp_no = titles.emp_no LEFT JOIN salaries ON employees.emp_no = salaries.emp_no LEFT JOIN dept_emp ON employees.emp_no = dept_emp.emp_no LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no WHERE employees.first_name = "${req.params.first_name}" AND salaries.to_date LIKE "9999%"`  
 
     pool.query(sql, (err, rows) => {
       if (err) return handleSQLError(res, err)
